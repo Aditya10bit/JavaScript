4f9f229e30ea4d409b17efd286480f9871c5d0e5
Aditya Das <152096526+Aditya10bit@users.noreply.github.com>
2025-03-09 16:53:57 +0000

    added code in ibj.js

--- .github/01_basics/obj.js
+++ .github/01_basics/obj.js
@@ -1,13 +1,17 @@
 //object literals 
 
 //to add a symbol in the object 
-const addr =  Symbol ("city")
 
- const jsUser ={
-    name : "Aditya",
-    age : 18,
-    [addr]:"Kolkata"//use as a symbol 
- }
+
+// const addr =  Symbol ("city")
+
+//  const jsUser ={
+//     name : "Aditya",
+//     age : 18,
+//     [addr]:"Kolkata"//use as a symbol 
+//  }
+
+
 /*
  //to access the objects 
  console.log(jsUser.age)
@@ -16,11 +20,24 @@ const addr =  Symbol ("city")
 */
 
 //to overwrite
-jsUser.age=21
-console.log(jsUser.age)
-jsUser.name="Aditya Das"
-Object.freeze(jsUser) //now no changes can be done in the object
-jsUser.name="Pratyush"
-console.log(jsUser)
+// jsUser.age=21
+// console.log(jsUser.age)
+// jsUser.name="Aditya Das"
+// Object.freeze(jsUser) //now no changes can be done in the object
+// jsUser.name="Pratyush"
+// console.log(jsUser)
+
+
+// jsUser.greeting= function() {
+//    console.log("Hello world")
+// }
+// console.log(jsUser.greeting)//function reference 
+// console.log(jsUser.greeting())//accesing the function from the object 
+
+// jsUser.greetingTwo=function(){
+//    console.log(`the name of the user is ${this.name}`)//String intercolation
+// }
+// console.log(jsUser.greetingTwo())
+
 
  
