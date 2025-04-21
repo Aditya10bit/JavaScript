fa2389f9e2ec8016304e41473aa18299cad54e13
Aditya das <adityadas270@gmail.com>
2025-04-21 06:24:11 +0530

    Modified the promise ,added fetch

--- .github/promise/promise1.js
+++ .github/promise/promise1.js
@@ -20,16 +20,16 @@
 //  })
 
  //catching errors 
- const promiseThree = new Promise(function(resolve,reject){
-    setTimeout(()=>{
-        let error = false
-        if(!error){
-            resolve({name:"aditya",email:"adityadas@gmail.com"})
-        }else{
-            reject("error !something went wrong!")
-        }
-    },1000)
- })
+//  const promiseThree = new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         let error = false
+//         if(!error){
+//             resolve({name:"aditya",email:"adityadas@gmail.com"})
+//         }else{
+//             reject("error !something went wrong!")
+//         }
+//     },1000)
+//  })
 
 //  promiseThree.then((user)=>{
 //     console.log(user)
@@ -43,20 +43,51 @@
 //  })
 
 
- //another approach using try catch block and async await 
-  async function PromiseThreeconsume() {
-    try{
-     // Await the promise and get the user object
-    const user = await promiseThree;
-    console.log(user); // Log the user object
-    // Extract the username from the user object
-    const username = user.name;
-    console.log(username); // Log the username
-    }catch(error){
-        console.log(error)
-    }finally {
-        console.log("promise is executed"); // Execute cleanup code
-      }
-  }
+//  //another approach using try catch block and async await 
+//   async function PromiseThreeconsume() {
+//     try{
+//      // Await the promise and get the user object
+//     const user = await promiseThree;
+//     console.log(user); // Log the user object
+//     // Extract the username from the user object
+//     const username = user.name;
+//     console.log(username); // Log the username
+//     }catch(error){
+//         console.log(error)
+//     }finally {
+//         console.log("promise is executed"); // Execute cleanup code
+//       }
+//   }
 
-  PromiseThreeconsume()
+  //PromiseThreeconsume()
+
+//   async function getAllUsers() {
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/users")
+//         const data = await response.json() //convert the string into json 
+        
+//         // Iterate over each user object in the array
+//         data.forEach(user => {
+//             console.log(`Name: ${user.name}, Email: ${user.email}`);
+//         });
+
+//     } catch (error) { 
+//         console.log(`E: ${error}`)
+//     }
+//   }
+//   getAllUsers()
+
+// doing the same code using then and catch
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((response)=>{
+    const data = response.json() //convert the string into json 
+    .then((data)=>{
+        data.forEach(user => {
+            console.log(`Name: ${user.name}, Email: ${user.email}`);
+        });
+    })   
+})
+.catch((error)=> console.log("error"))
+.finally(()=>{
+    console.log("promise completed")
+})
